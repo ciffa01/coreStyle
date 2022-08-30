83c43d44691832cda67dfe6557121b1a22ea5223
Simone Cefariello <simone.cefariello.01@gmail.com>
2022-08-30 11:05:07 +0200

    migliorie

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,40 +2,154 @@
 
 </script>
 <template>
+    <div class="flex f-row-s f-justify-center f-align-center f-wrap-l gap-l">
 
-    <div class="card flex f-column gap-xs cnt-s">
-        <span class="text-xs">Prova</span>
-        <div class="flex row-s gap-xs">
-            <div class="cnt-floating-label">
-                <input type="text" class="floating-input-100" placeholder="Nome">
-                <label class="floating-label-100">Nome</label>
+        <div class="card flex f-column gap-xs cnt-s">
+            <span class="text-m weight-500">Prova</span>
+            <div class="flex f-row-xs gap-xs">
+                <div class="cnt-floating-label">
+                    <input type="text" class="floating-input-100" placeholder="Nome">
+                    <label class="floating-label-100">Nome</label>
+                </div>
+        
+                <div class="cnt-floating-label">
+                    <input type="text" class="floating-input-100" placeholder="Cognome">
+                    <label class="floating-label-100">Cognome</label>
+                </div>
             </div>
     
-            <div class="cnt-floating-label">
-                <input type="text" class="floating-input-100" placeholder="Cognome">
-                <label class="floating-label-100">Cognome</label>
+            <div class="flex f-row-xs gap-xs">
+                <div class="cnt-floating-label">
+                    <input type="text" class="floating-input-100" placeholder=" ">
+                    <label class="floating-label-100">Prova</label>
+                </div>
+        
+                <div class="cnt-floating-label">
+                    <input type="text" class="floating-input-100" placeholder=" ">
+                    <label class="floating-label-100">Prova 2</label>
+                </div>
+    
+                <div class="cnt-floating-label">
+                    <input type="text" class="floating-input-100" placeholder="Ciao">
+                    <label class="floating-label-100">Prova 3</label>
+                </div>
+            </div>
+    
+            <div class="flex f-row gap-xs">
+                <button class="btn-default btn-blue-secondary">Salva</button>
+                <button class="btn-default btn-red-secondary">Annulla</button>
             </div>
         </div>
 
-        <div class="flex row-s gap-xs">
-            <div class="cnt-floating-label">
-                <input type="text" class="floating-input-100" placeholder=" ">
-                <label class="floating-label-100">Prova</label>
+        <div class="card flex f-column gap-xs cnt-s">
+            <span class="text-m weight-500">Prova</span>
+            <div class="flex f-row-xs gap-xs">
+                <div class="cnt-floating-label">
+                    <input type="text" class="floating-input-100" placeholder="Nome">
+                    <label class="floating-label-100">Nome</label>
+                </div>
+        
+                <div class="cnt-floating-label">
+                    <input type="text" class="floating-input-100" placeholder="Cognome">
+                    <label class="floating-label-100">Cognome</label>
+                </div>
             </div>
     
-            <div class="cnt-floating-label">
-                <input type="text" class="floating-input-100" placeholder=" ">
-                <label class="floating-label-100">Prova 2</label>
+            <div class="flex f-row-xs gap-xs">
+                <div class="cnt-floating-label">
+                    <input type="text" class="floating-input-100" placeholder=" ">
+                    <label class="floating-label-100">Prova</label>
+                </div>
+        
+                <div class="cnt-floating-label">
+                    <input type="text" class="floating-input-100" placeholder=" ">
+                    <label class="floating-label-100">Prova 2</label>
+                </div>
+    
+                <div class="cnt-floating-label">
+                    <input type="text" class="floating-input-100" placeholder="Ciao">
+                    <label class="floating-label-100">Prova 3</label>
+                </div>
             </div>
-
-            <div class="cnt-floating-label">
-                <input type="text" class="floating-input-100" placeholder="Ciao">
-                <label class="floating-label-100">Prova 3</label>
+    
+            <div class="flex f-row gap-xs">
+                <button class="btn-default btn-blue-secondary">Salva</button>
+                <button class="btn-default btn-red-secondary">Annulla</button>
             </div>
         </div>
 
-        <div>
-            <button class="btn-default btn-blue-secondary">Salva</button>
+        <div class="card flex f-column gap-xs cnt-s">
+            <span class="text-m weight-500">Prova</span>
+            <div class="flex f-row-xs gap-xs">
+                <div class="cnt-floating-label">
+                    <input type="text" class="floating-input-100" placeholder="Nome">
+                    <label class="floating-label-100">Nome</label>
+                </div>
+        
+                <div class="cnt-floating-label">
+                    <input type="text" class="floating-input-100" placeholder="Cognome">
+                    <label class="floating-label-100">Cognome</label>
+                </div>
+            </div>
+    
+            <div class="flex f-row-xs gap-xs">
+                <div class="cnt-floating-label">
+                    <input type="text" class="floating-input-100" placeholder=" ">
+                    <label class="floating-label-100">Prova</label>
+                </div>
+        
+                <div class="cnt-floating-label">
+                    <input type="text" class="floating-input-100" placeholder=" ">
+                    <label class="floating-label-100">Prova 2</label>
+                </div>
+    
+                <div class="cnt-floating-label">
+                    <input type="text" class="floating-input-100" placeholder="Ciao">
+                    <label class="floating-label-100">Prova 3</label>
+                </div>
+            </div>
+    
+            <div class="flex f-row gap-xs">
+                <button class="btn-default btn-blue-secondary">Salva</button>
+                <button class="btn-default btn-red-secondary">Annulla</button>
+            </div>
+        </div>
+
+        <div class="card flex f-column gap-xs cnt-s">
+            <span class="text-m weight-500">Prova</span>
+            <div class="flex f-row-xs gap-xs">
+                <div class="cnt-floating-label">
+                    <input type="text" class="floating-input-100" placeholder="Nome">
+                    <label class="floating-label-100">Nome</label>
+                </div>
+        
+                <div class="cnt-floating-label">
+                    <input type="text" class="floating-input-100" placeholder="Cognome">
+                    <label class="floating-label-100">Cognome</label>
+                </div>
+            </div>
+    
+            <div class="flex f-row-xs gap-xs">
+                <div class="cnt-floating-label">
+                    <input type="text" class="floating-input-100" placeholder=" ">
+                    <label class="floating-label-100">Prova</label>
+                </div>
+        
+                <div class="cnt-floating-label">
+                    <input type="text" class="floating-input-100" placeholder=" ">
+                    <label class="floating-label-100">Prova 2</label>
+                </div>
+    
+                <div class="cnt-floating-label">
+                    <input type="text" class="floating-input-100" placeholder="Ciao">
+                    <label class="floating-label-100">Prova 3</label>
+                </div>
+            </div>
+    
+            <div class="flex f-row gap-xs">
+                <button class="btn-default btn-blue-secondary">Salva</button>
+                <button class="btn-default btn-red-secondary">Annulla</button>
+            </div>
         </div>
     </div>
 </template>
